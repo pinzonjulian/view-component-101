7027eade81b4e5d6cf98ba7ae74c5843737aa255
Julian Pinzon <10928518+pinzonjulian@users.noreply.github.com>
2020-06-22 22:39:49 +1000

    fix: enhance styles

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,4 +2,21 @@ class Invitation < ApplicationRecord
   belongs_to :user
   belongs_to :party
   has_one :location, through: :party
+
+  enum state: {
+    busy_that_day: -10,
+    too_far_away: -9,
+    pending: 0,
+    arriving_late: 8,
+    dying_to_go: 9,
+    prepared_to_go: 10
+  }
+
+  def map_transit_predictions
+    {
+      walking: "#{rand(40..50)} days",
+      horse: "#{rand(20..35)} days",
+      dragon: "#{rand(12..66)} hours",
+    }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_person_name
+
   validates_presence_of :first_name, :last_name
   has_one :location, as: :locatable
 
